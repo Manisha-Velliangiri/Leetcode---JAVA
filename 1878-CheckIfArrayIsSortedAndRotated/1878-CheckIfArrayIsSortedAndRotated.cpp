c38536166fbc998f5d3d8117b6d359a8baadd3d7
Manisha.V <128169122+Manisha-Velliangiri@users.noreply.github.com>
2025-07-22 15:14:28 +0530

    Create 1878-CheckIfArrayIsSortedAndRotated

---
+++ 1878-CheckIfArrayIsSortedAndRotated/1878-CheckIfArrayIsSortedAndRotated.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/22/2025, 3:14:28 PM
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>nums[(i+1) % nums.size()]){
+                count++;
+            }
+        }
+        if(count>1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
+};
