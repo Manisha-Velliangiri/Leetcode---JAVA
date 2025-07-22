b6d5422aec7e206baed8f6aafe374dad1fecf44b
Manisha.V <128169122+Manisha-Velliangiri@users.noreply.github.com>
2025-07-22 15:14:57 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/22/2025, 3:14:57 PM
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int profit=0;
+
+        for(int i=1;i<prices.size();i++){
+            int cost= prices[i]-mini;
+            mini=min(mini,prices[i]);
+            
+            profit=max(profit,cost);
+        }
+        return profit;
+    }
+};
