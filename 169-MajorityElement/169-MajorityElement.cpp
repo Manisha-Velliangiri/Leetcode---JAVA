abfaffc3baea34538441556b9cc3293a6bd41a22
Manisha.V <128169122+Manisha-Velliangiri@users.noreply.github.com>
2025-07-22 15:14:50 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/22/2025, 3:14:50 PM
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority=nums[0];
+        int count=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==majority){
+                count+=1;
+            }
+            else{
+                count-=1;
+                if(count==0){
+                    majority=nums[i];
+                    count+=1;
+                }
+            }
+        }
+        return majority;
+    }
+};
