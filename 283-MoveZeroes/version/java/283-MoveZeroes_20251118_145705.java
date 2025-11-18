24f63484810a50e7a778834b8d461a59c7438486
Manisha.V <128169122+Manisha-Velliangiri@users.noreply.github.com>
2025-11-18 14:57:04 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/version/java/283-MoveZeroes_20251118_145705.java
@@ -0,0 +1,21 @@
+// Last updated: 11/18/2025, 2:57:05 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+    
+        int j=0;
+
+        for(int i=0;i<nums.length;i++){
+          
+                if(nums[i]!=0){
+                   
+                   int temp=nums[i];
+                   nums[i]=nums[j];
+                   nums[j]=temp;
+
+                   j++;
+                }
+
+        }
+    }
+}
